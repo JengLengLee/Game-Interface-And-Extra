a43a98212145c98fdf39aafa4a675381b54b11bf
Jeng Leng Lee <43627447+JengLengLee@users.noreply.github.com>
2018-12-05 00:22:25 -0600

    Add files via upload

--- Interface/MainWindow.xaml.cs
+++ Interface/MainWindow.xaml.cs
@@ -1,45 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Interface
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class
+        /// </summary>
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
-        private void btnSudoku_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Click event that starts a new form
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param> 
+        private void BtnSudoku_Click(object sender, RoutedEventArgs e)
         {
             // use to show the form for the sudoku game.
             SudokuGame.MainWindow sudoku = new SudokuGame.MainWindow();
             sudoku.ShowDialog();
         }
 
-        private void btnMancala_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Click event that starts a new form
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param> 
+        private void BtnMancala_Click(object sender, RoutedEventArgs e)
         {
             // use to show the form for the sudoku game.
             Mancala.MainWindow mancala = new Mancala.MainWindow();
             mancala.ShowDialog();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Click event that exits program
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param> 
+        private void Button1_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
